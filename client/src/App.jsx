995de1b0382bc337a9a4b4de5da2813466782da6
rupam <rupamdey#gmail.com>
2024-10-27 21:22:31 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-  <>
-  <h1 ></h1>
-  </>
+    <div>App</div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import DashBoard from './pages/DashBoard.jsx';
+import Projects from './pages/Projects.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path= "/" element={<Home />}/>
+    <Route path= "/about" element={<About />}/>
+    <Route path= "/signin" element={<SignIn/>}/>
+    <Route path= "/signup" element={<SignUp />}/>
+    <Route path= "/dashboard" element={<DashBoard />}/>
+    <Route path= "/projects" element={<Projects />}/>
+    
+
+
+    </>
+
+      
+    
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
