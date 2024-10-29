91f6ec84cddeb9e1c801b1cb12c2c6c137bbcaa7
rupam <rupamdey#gmail.com>
2024-10-30 00:03:44 +0530

    "add db"

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// Ensure that you are using process.env.MONGODB_URI
+mongoose.connect(process.env.MONGODB_URI, )
+    .then(() => {
+        console.log("Connected to MongoDB!");
+
+        // Start the server only after a successful connection
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.error("Connection error:", error);
+    });
