be0198585389d9b2955b2f42061d0d48c10122d5
rupam <rupamdey#gmail.com>
2024-10-31 18:21:22 +0530

    ADD A MIDDLEWARE AND EROOR FUN

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,18 @@
 import user from "../models/user.model.js";
 import bcrypt from 'bcrypt';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     // Validate the input fields
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "Fields are required" });
+    next(errorHandler(400,"All fields are required."))
     }
 
     try {
         // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10); // Use bcrypt.hash here
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         // Create a new user instance with the hashed password
         const newUser = new user({ username, email, password: hashedPassword });
@@ -20,6 +21,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json({ message: "User registered successfully" });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        next(error); // Pass the error to the error-handling middleware
     }
 };
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 // Ensure that you are using process.env.MONGODB_URI
-mongoose.connect(process.env.MONGODB_URI, )
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("Connected to MongoDB!");
 
@@ -22,6 +22,20 @@ mongoose.connect(process.env.MONGODB_URI, )
     })
     .catch((error) => {
         console.error("Connection error:", error);
+        process.exit(1); // Exit process if unable to connect to MongoDB
     });
-    app.use('/api/user',userRoutes)
-    app.use('/api/auth',authRoutes)
+
+// Route middleware
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
