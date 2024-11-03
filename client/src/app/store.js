93dfe6b5501c169e775c11a25885a7a0afd89e6d
rupam <rupamdey#gmail.com>
2024-11-03 21:28:35 +0530

    REDUX_PERSIST

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../redux/user/userSlice';
+import { combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store = configureStore({
-    reducer: { user: userReducer },
+const rootReducer = combineReducers({
+    user: userReducer,
 });
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+// Pass persistConfig and rootReducer as separate arguments
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
 export default store;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,35 +11,30 @@ import DashBoard from './pages/DashBoard.jsx';
 import Projects from './pages/Projects.jsx';
 import Layout from './Layout.jsx';
 import { Provider } from 'react-redux';
-import  store  from './app/store.js';
-
+import store, { persistor } from './app/store.js'; // Import both store and persistor correctly
+import { PersistGate } from 'redux-persist/integration/react';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    
     <>
-    <Route path="/" element={<Layout/>}>
-    <Route path= "" element={<Home />}/>
-    <Route path= "about" element={<About />}/>
-    <Route path= "signin" element={<SignIn/>}/>
-    <Route path= "signup" element={<SignUp />}/>
-    <Route path= "dashboard" element={<DashBoard />}/>
-    <Route path= "projects" element={<Projects />}/>
-    
-    </Route>
-
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="dashboard" element={<DashBoard />} />
+        <Route path="projects" element={<Projects />} />
+      </Route>
     </>
-
-      
-    
   )
 );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-
-    <RouterProvider router={router} />
+    <Provider store={store}> {/* Wrap PersistGate inside Provider */}
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
